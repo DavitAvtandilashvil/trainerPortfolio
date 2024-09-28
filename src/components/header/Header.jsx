6fe2156a1e15e6686f5365f282b2f6bd9a67d289
marriamdd <davitashvili.m3@gmail.com>
2024-09-28 20:14:10 +0400

    style:add header &layout murkups

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,68 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="flex p-[20px]">
-      <img className="mr-auto" src="" alt="logo" />
-      <div className="flex gap-[10px]">
-        <Link to={"/"}>HOME</Link>
-        <Link to={"/stories"}> STORIES</Link>
-        <Link to={"/services"}>SERVICES</Link>
-        <Link to={"/contact"}>CONTACT</Link>
+    <div className="flex ">
+      <Link className="flex gap-[1.6rem] mr-auto" to={"/"}>
+        <img
+          className="w-[8.8rem] h-[5.8rem]"
+          src="/testLogo.svg"
+          alt="textLogo"
+        />
+
+        <img
+          className="hidden md:flex md:w-[5.2rem] md:h-[5.8rem] w-[7.4rem] h-[4.8rem]"
+          src="/locoIcon.svg"
+          alt="logo"
+        />
+      </Link>
+
+      <div className="flex border border-[#4D4D4D] bg-[#222222] py-[10px] px-[20px] items-center rounded-[20rem] gap-[68px] text-[1.6rem] font-[400] text-[white]">
+        <NavLink
+          className={({ isActive }) =>
+            `rounded-[48px] font-[400] py-[13px] transition-all duration-[1s]  easy-in-out ${
+              isActive &&
+              "lg:px-[3.4rem] px-[4.8rem] text-[black] font-[700] bg-[#D7FD44] "
+            }`
+          }
+          to={"/"}
+        >
+          HOME
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `rounded-[48px] font-[400] transition-all duration-[1s] py-[13px]  easy-in-out ${
+              isActive &&
+              "lg:px-[3.4rem] px-[4.8rem]  text-[black] font-[700] bg-[#D7FD44] "
+            }`
+          }
+          to={"/stories"}
+        >
+          {" "}
+          STORIES
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `rounded-[48px] font-[400] transition-all duration-[1s]  py-[13px] easy-in-out ${
+              isActive &&
+              "lg:px-[3.4rem] px-[4.8rem]  text-[black] font-[700] bg-[#D7FD44] "
+            }`
+          }
+          to={"/services"}
+        >
+          SERVICES
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `rounded-[48px] font-[400] transition-all duration-[1s] py-[13px] easy-in-out  ${
+              isActive &&
+              "lg:px-[3.4rem] px-[4.8rem]  text-[black] font-[700] bg-[#D7FD44]"
+            }`
+          }
+          to={"/contact"}
+        >
+          CONTACT
+        </NavLink>
       </div>
     </div>
   );
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -4,12 +4,12 @@ import Footer from "../components/footer/Footer";
 
 export default function Layout() {
   return (
-    <>
+    <div className="pt-[5rem] px-[8rem]">
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </>
+    </div>
   );
 }
