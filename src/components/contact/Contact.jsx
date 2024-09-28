b9aa3b7dce56ed95dc20c5f1b64ab4cd86617d37
marriamdd <davitashvili.m3@gmail.com>
2024-09-28 23:23:10 +0400

    feat:react hook form with yup

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,3 +1,96 @@
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 export default function Contact() {
-  return <div>Contact</div>;
+  const validationSchema = yup.object().shape({
+    fullName: yup
+      .string()
+      .required("Full Name is required")
+      .min(2, "Full Name must be at least 2 characters"),
+    email: yup
+      .string()
+      .required("Email is required")
+      .email("Must be a valid email"),
+    phoneNumber: yup
+      .string()
+      .required("Phone Number is required")
+      .matches(/^\d+$/, "Phone Number must contain only numbers"),
+    message: yup
+      .string()
+      .required("Message is required")
+      .min(5, "Message must be at least 5 characters long"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+  });
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className="md:w-[522px] pt-[4rem] pb-[8.8rem] w-[256px]">
+      <h2 className="pb-[1.6rem]">CONTACT</h2>
+
+      <form
+        className="flex flex-col text-white"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+        <input
+          className="bg-[#222222] rounded-[.8rem]  px-[1rem] h-[4.2rem] border border-[#4D4D4D] mb-2"
+          placeholder="Full Name"
+          {...register("fullName")}
+        />
+        {errors.fullName && (
+          <span className="text-[#f44040] py-[.3rem]">
+            {errors.fullName.message}
+          </span>
+        )}
+
+        <input
+          className="bg-[#222222] rounded-[.8rem] px-[1rem] h-[4.2rem] text-white border border-[#4D4D4D] mb-2"
+          placeholder="Email"
+          {...register("email")}
+        />
+        {errors.email && (
+          <span className="text-[#f44040] py-[.3rem]">
+            {errors.email.message}
+          </span>
+        )}
+
+        <input
+          className="bg-[#222222] rounded-[.8rem] px-[1rem] h-[4.2rem] text-white border border-[#4D4D4D] mb-2"
+          placeholder="Phone Number"
+          {...register("phoneNumber")}
+        />
+        {errors.phoneNumber && (
+          <span className="text-[#f44040] py-[.3rem]">
+            {errors.phoneNumber.message}
+          </span>
+        )}
+
+        <input
+          className="bg-[#222222] h-[4.2rem] px-[1rem] rounded-[.8rem] text-white border border-[#4D4D4D] mb-2"
+          placeholder="Message"
+          {...register("message")}
+        />
+        {errors.message && (
+          <span className="text-[#f44040] py-[.3rem]">
+            {errors.message.message}
+          </span>
+        )}
+
+        <input
+          className="bg-[red] h-[4.2rem] rounded-[.8rem]"
+          type="submit"
+          value="Submit"
+        />
+      </form>
+    </div>
+  );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import NavLinks from "../sharedComponents/NavLinks";
 
 export default function Header() {
   return (
-    <header className="flex md:pt-[5rem] md:px-[8rem] pt-[4rem] px-[4rem] ">
+    <header className="flex md:pt-[5rem] md:px-[8rem] pt-[4rem] items-center px-[4rem] ">
       <Link className="flex gap-[1.6rem] mr-auto" to={"/"}>
         <img
           className="w-[8.8rem] h-[5.8rem]"
