4aaa2b7e3a8c3a93ccb8dd1940942ee1143c1f51
merabkandelaki <100033268+merabkandelaki@users.noreply.github.com>
2024-10-05 14:33:01 +0400

    change style in ServicesPage

--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -4,7 +4,7 @@ import Prices from "../components/prices/Prices";
 export default function ServicesPage() {
   return (
     <div>
-      <JourneyScroll title="my services" className="pt-0 px-20 pb-20" />
+      <JourneyScroll title="my services" className="pt-20 px-20 pb-20" />
       <Prices />
     </div>
   );
