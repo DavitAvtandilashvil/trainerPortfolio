077244fdddb2d5a69d084c603e705f854bd78dd5
marriamdd <davitashvili.m3@gmail.com>
2024-10-05 16:58:50 +0400

    feat:implement react query & supabase

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,16 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+const queryClient = new QueryClient();
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools />
+    <BrowserRouter>
+      <StrictMode>
+        <App />
+      </StrictMode>
+    </BrowserRouter>
+  </QueryClientProvider>
 );
